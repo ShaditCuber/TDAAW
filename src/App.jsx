8591aff4e3903f46859a711b96342c1af342cb72
Felipe Bastidas <64512945+ShaditCuber@users.noreply.github.com>
2023-09-26 20:12:42 -0400

    Prueba Imagen guau

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import DogCard from './components/DogCard'
 
 function App() {
   return (
     <>
-  
-      <DogCard name={'ejemplo'} image={'https:\/\/images.dog.ceo\/breeds\/frise-bichon\/3.jpg'} description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem laboriosam, aperiam maxime cumque optio modi delectus blanditiis nobis omnis iure minima, quod architecto consectetur ullam dicta accusamus. Vitae, qui dolores.'}></DogCard>
+      <DogCard name={'ejemplo'} image={'https:/images.dog.ceo/breeds/frise-bichon/3.jpg'} description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem laboriosam, aperiam maxime cumque optio modi delectus blanditiis nobis omnis iure minima, quod architecto consectetur ullam dicta accusamus. Vitae, qui dolores.'}></DogCard>
     </>
   )
 }
--- src/components/DogCard.jsx
+++ src/components/DogCard.jsx
@@ -1,6 +1,7 @@
 
 // eslint-disable-next-line react/prop-types
 export default function DogCard({ name, description, image }) {
+    console.log(name)
     return (
         <div className="card">
             <img src={image} alt={name} />
